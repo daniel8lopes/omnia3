11ff253af53147afcd136dda5f80853d9ac07d27
DiogoTx <deefilipe@gmail.com>
2018-07-27 11:22:05 +0100

    Update customscripts.js

--- docs/js/customscripts.js
+++ docs/js/customscripts.js
@@ -3,7 +3,9 @@ $('#mysidebar').height($(".nav").height());
 
 $( document ).ready(function() {
 
-    alert("Hello!");
+if(document.baseURI.contains("omnia3_platformchangelog.html")){
+alert("Hello");
+}
     //this script says, if the height of the viewport is greater than 800px, then insert affix class, which makes the nav bar float in a fixed
     // position as your scroll. if you have a lot of nav items, this height may not work for you.
     var h = $(window).height();
